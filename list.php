6488aada8abfd38fdbb4f412910e431d0d92e71f
Marcin <mnowinski01@protonmail.com>
2021-04-08 19:19:55 +0200

    changes to html syntax and rewritten css

--- list.php
+++ list.php
@@ -15,8 +15,15 @@
 </head>
 <body>
     <div id="list">
-        <a id="goback-button" href="index.php">Go back</a>
-        <center><h1 class="global_title">Home library</h1></center>
+        <a id="list_goback_button" href="index.php">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-left" width="20" height="20 viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <line x1="5" y1="12" x2="19" y2="12" />
+            <line x1="5" y1="12" x2="11" y2="18" />
+            <line x1="5" y1="12" x2="11" y2="6" />
+        </svg>Back
+        </a>
+        <h1 class="list_title">Home library</h1>
         <?php
             try {
                 $conn = new mysqli('localhost', 'root', '', 'library');
@@ -28,84 +35,56 @@
                     if($_GET["s"] == ""){
                         $genre = $_GET["g"];
                         $query = "SELECT * FROM books WHERE genre = '$genre'";
-                        echo "<center><h2 class='global_subtitle'>Books of genre "."<span class='bluebg'>".$genre."</span>"."</h2> </center>";
+                        echo "<center><h2 class='list_subtitle'>Books of genre "."<span class='list_subtitle_highlight'>".$genre."</span>"."</h2> </center>";
                     }
 
                     if($_GET["g"] == "") {
                         $search = $_GET["s"];
                         $query = "SELECT * FROM books WHERE author LIKE '%%$search%%' OR other_authors LIKE '%%$search%%' OR title LIKE '%%$search%%' OR publishing_house LIKE '%%$search%%' OR original_title LIKE '%%$search%%'";
-                        echo "<center><h2 class='global_subtitle'>Books which contains the: &raquo".$search."&laquo</h2></center>";
+                        echo "<center><h2 class='list_subtitle'>Books which contains the: &raquo".$search."&laquo</h2></center>";
                     }
 
                     if($_GET["s"] == "" && $_GET["g"] == "") {
                         header("Location: index.php");
                     }
 
-                    if(($_GET["s"] && $_GET["g"]) != "") {
+                    if(($_GET["s"] && $_GET["g"]) != "" && strlen($_GET["s"]) > 2) {
                         $genre = $_GET["g"];
                         $search = $_GET["s"];
                         $query = "SELECT * FROM books WHERE genre LIKE '$genre' AND author LIKE '%%$search%%' OR other_authors LIKE '%%$search%%' OR title LIKE '%%$search%%' OR publishing_house LIKE '%%$search%%' OR original_title LIKE '%%$search%%'";
-                        echo "<center><h2 class='global_subtitle'>Books which contains the: &raquo".$search."&laquo where genre is: <span class='bluebg'>".$genre."</span></h2></center>";
+                        echo "<center><h2 class='list_subtitle'>Books which contains the: &raquo".$search."&laquo where genre is: <span class='list_subtitle_highlight'>".$genre."</span></h2></center>";
                     }
 
-                    $result = $conn->query($query);
-
-                    while(($row = $result -> fetch_assoc()) != 0) {
-                        echo "<div onclick='showInfo(".$row['id'].",1)' class='item'>";
-                            echo $row['id'];
-                            echo "<h2 class='title'>";
-                                echo $row['title'];
-                                echo " ";
-                                echo "<span class='date'>";
-                                if ($row['release_year'] == 0){
-                                    echo " ";
-                                } else {
-                                    echo $row['release_year'];
-                                }
-                                echo "</span>";
-                            echo "</h2>";
-                            echo "<h3 class='author'>";
-                                if($row['other_authors'] !== ""){
-                                    echo $row['author'].", ".$row['other_authors'];
-                                } else {
-                                    echo $row['author'];
-                                }
-                            echo "</h3>";
-                            echo "<div id='" . $row['id'] . "' class='info-container'>";
-                                echo "Pages: ";
-                                echo $row['pages'];
-                                if ($row['first_release'] == 0){
-                                    echo "";
-                                } else {
-                                    echo ", First release: " . $row['first_release'];
-                                }
-                                if ($row['original_title'] == ""){
-                                    echo "";
-                                } else {
-                                    echo ", Original title: <i>" . $row['original_title'] . "</i>";
-                                }
-                                echo ", ";
-                                echo "Price: ";
-                                if($row['price'] == 0.00){
-                                echo "<i>price not found</i>";
-                                }else {
-                                echo $row['price']." zł";
-                                }
-                                echo ", Genre: ";
-                                echo $row['genre'];
-                                if($row['publishing_house'] == ""){
-                                    echo " ";
-                                } else {
-                                    echo ", Publishing house: ".$row['publishing_house'];
-                                }
-                            echo "</div>";
-                        echo "</div>";
+                    $result = $conn->query($query); //executing query
+                    while(($row = $result -> fetch_assoc()) != 0) { //writing out all records
+        ?>
+        <div class="list_item">
+            <?php echo $row['id'];?>
+            <h2 class="list_item_title">
+                <?php echo $row['title']." ";?>
+                <span class="list_item_date">
+                    <?php if ($row['release_year'] == 0){ echo " ";} else { echo $row['release_year'];}?>
+                </span>
+            </h2>
+            <h3 class="list_item_author">
+                <?php if($row['other_authors'] !== ""){ echo $row['author'].", ".$row['other_authors']; } else { echo $row['author'];}?>
+            </h3>
+            <div id="<?php $row['id'];?>" class="list_item_info">
+                Pages: <?php echo $row['pages'];?>
+                <?php
+                    if ($row['first_release'] == 0){ echo "";} else { echo "| First release: " . $row['first_release'];}
+                    if ($row['original_title'] == ""){ echo "";} else { echo "| Original title: <i>".$row['original_title'] . "</i>";}
+                ?>
+                Price: <?php if($row['price'] == 0.00){ echo ""; }else { echo $row['price']." zł";} ?>
+                Genre: <?php echo $row['genre']; ?>
+                <?php if($row['publishing_house'] == ""){ echo ""; } else {echo ", Publishing house: ".$row['publishing_house'];}?>
+            </div>
+        </div>
+        <?php
                     }
                 }
                 $conn -> close();
-            } catch (Exception $e) {
-                echo "Error ".$e;
-            }
+                } catch (Exception $e) { echo "Error ".$e; }
         ?>
     </div>
     <script src="js/list.js"></script>
