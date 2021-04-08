017dbf257451f74477850cd505dd5176b99d6ad0
Marcin <mnowinski01@protonmail.com>
2021-04-08 19:20:21 +0200

    adjustment to new css

--- js/list.js
+++ js/list.js
@@ -1,8 +1,9 @@
-const divs = [...document.querySelectorAll('.item')]
+const divs = [...document.querySelectorAll('.list_item')]
 divs.forEach(div => div.addEventListener('click', e => {
     // divs.forEach(div => {
     //     div.querySelector('.info-container').classList.remove('active')
     // })
-    const infoDiv = div.querySelector('.info-container')
+    const infoDiv = div.querySelector('.list_item_info')
     infoDiv.classList.toggle('active')
+    infoDiv.style.animation = "0.5s slideDown";
 }))
