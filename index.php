ccea829d4b79da53b7ee6612c8eea805b9009d55
Marcin <mnowinski01@protonmail.com>
2021-04-29 15:21:15 +0200

    fix to reset button on mobile

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
                     <line x1="21" y1="21" x2="15" y2="15" />
                 </svg>
             </button>
-            <button type="reset" id="index_form_clear_button">
+            <button type="reset" id="index_form_reset_button">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <line x1="18" y1="6" x2="6" y2="18" />
--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 const resetButton = document.querySelector("#index_form_reset_button");
 const searchBar = document.querySelector("#index_form_searchbar");
+const genreList = document.querySelector("#index_form_genrelist");
 const genres = document.querySelectorAll("option");
 
-const checkSearchBarValue = () => {
+searchBar.addEventListener("keyup", () => {
     //check if status bar is empty
     if (searchBar.value == "" && genres.value == ""){
         //if it is, apply display: none and timeout for animation
@@ -13,24 +14,21 @@ const checkSearchBarValue = () => {
         resetButton.style.display = "flex";
         resetButton.style.animation = ".5s fadeUp";
     }
-}
+})
 
-//set 'for' loop to apply event listener for all option elements
-for (var i = 0; i < genres.length; i++){
-    genres[i].addEventListener("click", () => {
-        //check if genre list is empty
-        if (genres[i] == ""){
-            //if it is, apply display: none and timeout for animation
-            resetButton.style.animation = ".5s fadeDown";
-            setTimeout(() => resetButton.style.display = "none", 500);
-        } else {
-            //if it isn't, apply display: flex and animation
-            resetButton.style.display = "flex";
-            resetButton.style.animation = ".5s fadeUp";
-        }
-    });
-}
-searchBar.addEventListener("keyup", checkSearchBarValue);
+genreList.addEventListener("input", () => {
+    //check if genre list is empty
+    if (genreList == ""){
+        //if it is, apply display: none and timeout for animation
+        resetButton.style.animation = ".5s fadeDown";
+        setTimeout(() => resetButton.style.display = "none", 500);
+    } else {
+        //if it isn't, apply display: flex and animation
+        resetButton.style.display = "flex";
+        resetButton.style.animation = ".5s fadeUp";
+    };
+})
+
 resetButton.addEventListener("click",() => {
     resetButton.style.animation = ".5s fadeDown";
     //set timeout for applying display: none
