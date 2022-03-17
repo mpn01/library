2ca74b13b1e92719502330de23b81aab12cd3d68
Marcin <mnowinski01@protonmail.com>
2022-03-17 21:32:34 +0100

    Update index.php - changing icons in search form Update index.css - update properties for search form items

--- index.php
+++ index.php
@@ -16,16 +16,21 @@
         <label for="global_darkmode_toggle" id="global_darkmode_toggle_label"></label>
     </div>
     <div id="index">
-        <h1 class="main_title">Home library</h1>
+        <h1 class="main_title">Library</h1>
         <h3 class="main_subtitle">search for your book</h3>
         <form action="list.php" method="GET" id="index_form">
             <div tabindex="0" id="index_form_searchbox">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#948f8f" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#948f8f" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
                     <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" />
+                </svg> -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="23" height="23" viewBox="0 0 24 24" stroke-width="1.5" stroke="#948f8f" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <circle cx="10" cy="10" r="7" />
+                    <line x1="21" y1="21" x2="15" y2="15" />
                 </svg>
-                <input tabindex="0" name="s" id="index_form_searchbar" type="text" placeholder="title, author or tag">
+                <input tabindex="0" name="s" id="index_form_searchbar" type="text" placeholder="author, title or tag">
             </div>
             <select name="g" id="index_form_genrelist">
                 <option value="" disabled selected>choose a genre</option>
@@ -50,16 +55,16 @@
                     $conn -> close();
                 ?>
             </select>
-            <a href="list.php?a" id="index_form_showall_button">Show all books</a>
+            <a href="list.php?a" id="index_form_showall_button">Show all</a>
             <button id="index_form_submit_button" type="submit">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="32" height="32" viewBox="0 0 24 24" stroke-width="2 " stroke="#efefef" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#efefef" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <circle cx="10" cy="10" r="7" />
                     <line x1="21" y1="21" x2="15" y2="15" />
                 </svg>
             </button>
             <button type="reset" id="index_form_reset_button">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="#efefef" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <line x1="18" y1="6" x2="6" y2="18" />
                 <line x1="6" y1="6" x2="18" y2="18" />
