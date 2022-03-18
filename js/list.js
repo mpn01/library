eab5c9e6dfc63fb6519a0cafe3ed967dfdf8cf0c
Marcin <mnowinski01@protonmail.com>
2022-03-18 19:14:35 +0100

    Update list.js, list.php, list.css

--- js/list.js
+++ js/list.js
@@ -1,13 +1,7 @@
-// const listItems = [...document.querySelectorAll('.list_item')];
-// listItems.forEach(listItem => listItem.addEventListener('click', () => {
-//     const listItemSidebar = document.querySelector('.list_item_sidebar');
-//     listItemSidebar.classList.toggle('active');
-// }))
-
-const listItemShowallButtons = [...document.querySelectorAll('.list_item_showmore_button')];
-listItemShowallButtons.forEach(listItemShowallButton => listItemShowallButton.addEventListener('click', () => {
-    const listItemSidebar = document.querySelector('.list_item_sidebar');
-    listItemSidebar.classList.toggle('active');
+const listItems = [...document.querySelectorAll('.list_item')];
+listItems.forEach(listItem => listItem.addEventListener('click', () => {
+    const listItemInfo = document.querySelector('.list_item_info');
+    listItemInfo.classList.toggle('active');
 }))
 
 const goBack = () => {
--- list.php
+++ list.php
@@ -67,22 +67,14 @@
             <h3 class="list_item_author">
                 <?php if($row['other_authors'] !== ""){ echo "<a href='#'>".$row['author']."</a> ".$row['other_authors']; } else { echo "<a href='author.php?a=".$row['author']."'>".$row['author']."</a>";}?>
             </h3>
-            <div class="list_item_showmore_button">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dots-vertical" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <circle cx="12" cy="12" r="1"></circle>
-                    <circle cx="12" cy="19" r="1"></circle>
-                    <circle cx="12" cy="5" r="1"></circle>
-                </svg>
+            <div class="list_item_info">
+                Pages: <?php echo $row['pages'].", ";?>
+                <?php if ($row['first_release'] == 0){ echo "";} else { echo "First release: " . $row['first_release'].", ";}
+                if ($row['original_title'] == ""){ echo "";} else { echo "Original title: <i>".$row['original_title'] . "</i>".", ";}?>
+                Price: <?php if($row['price'] == null){ echo ""; }else { echo $row['price']." zł, ";} ?>
+                Genre: <?php echo $row['genre'].", "; ?>
+                <?php if($row['publishing_house'] == ""){ echo ""; } else {echo "Publishing house: ".$row['publishing_house'];}?>
             </div>
-        </div>
-        <div class="list_item_sidebar">
-            Pages: <?php echo $row['pages'].", ";?>
-            <?php if ($row['first_release'] == 0){ echo "";} else { echo "First release: " . $row['first_release'].", ";}
-            if ($row['original_title'] == ""){ echo "";} else { echo "Original title: <i>".$row['original_title'] . "</i>".", ";}?>
-            Price: <?php if($row['price'] == null){ echo ""; }else { echo $row['price']." zł, ";} ?>
-            Genre: <?php echo $row['genre'].", "; ?>
-            <?php if($row['publishing_house'] == ""){ echo ""; } else {echo "Publishing house: ".$row['publishing_house'];}?>
         </div>
         <?php
                     }
