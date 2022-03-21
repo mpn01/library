9dc67bf09d0d9ee103bd7a0609d61bfcfb404b98
Marcin <mnowinski01@protonmail.com>
2022-03-21 15:40:57 +0100

    Update list.php, list.css

--- list.php
+++ list.php
@@ -1,6 +1,6 @@
 ﻿<html data-theme="light">
 <head>
-    <meta charset="UTF-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="assets/img/icons/favicon.svg">
     <link rel="stylesheet" type="text/css" href="styles/css/main.css"/>
@@ -57,24 +57,32 @@
                     while(($row = $result -> fetch_assoc()) != 0) {
         ?>
         <div class="list_item">
-            <span class="list_item_id"><?php echo $row['id'];?></span>
-            <h2 class="list_item_title">
-                <?php echo $row['title']." ";?>
-                <span class="list_item_date">
-                    <?php if ($row['release_year'] == 0){ echo " ";} else { echo $row['release_year'];}?>
-                </span>
-            </h2>
-            <h3 class="list_item_author">
-                <?php if($row['other_authors'] !== ""){ echo "<a href='#'>".$row['author']."</a> ".$row['other_authors']; } else { echo "<a href='author.php?a=".$row['author']."'>".$row['author']."</a>";}?>
-            </h3>
-            <div class="list_item_info">
-                Pages: <?php echo $row['pages'].", ";?>
-                <?php if ($row['first_release'] == 0){ echo "";} else { echo "First release: " . $row['first_release'].", ";}
-                if ($row['original_title'] == ""){ echo "";} else { echo "Original title: <i>".$row['original_title'] . "</i>".", ";}?>
-                Price: <?php if($row['price'] == null){ echo ""; }else { echo $row['price']." zł, ";} ?>
-                Genre: <?php echo $row['genre'].", "; ?>
-                <?php if($row['publishing_house'] == ""){ echo ""; } else {echo "Publishing house: ".$row['publishing_house'];}?>
-                <img class="list_item_info_cover" src="assets/covers/placeholder.png"/>
+            <div class="list_item_">
+                <span class="list_item_id"><?php echo $row['id'];?></span>
+                <h2 class="list_item_title">
+                    <?php echo $row['title']." ";?>
+                    <span class="list_item_date">
+                        <?php if ($row['release_year'] == 0){ echo " ";} else { echo $row['release_year'];}?>
+                    </span>
+                </h2>
+                <h3 class="list_item_author">
+                    <?php if($row['other_authors'] !== ""){ echo "<a href='#'>".$row['author']."</a> ".$row['other_authors']; } else { echo "<a href='author.php?a=".$row['author']."'>".$row['author']."</a>";}?>
+                </h3>
+            </div>
+            <div class="list_item_info"> 
+                <div id="column-1">
+                    <div class="list_item_info_pages"><b>Pages</b> <?php echo $row['pages'];?> </div>
+                    <div class="list_item_info_price"><b>Price</b> <?php if($row['price'] == null){ echo ""; }else { echo $row['price']." zł";} ?> </div>
+                    <div class="list_item_info_release"><?php if ($row['first_release'] == 0){ echo "";} else { echo "<b>First release</b> " . $row['first_release'];}?> </div>
+                </div>
+                <div id="column-2">
+                    <div class="list_item_info_genre"><b>Genre </b><?php echo $row['genre'];?></div>
+                    <div class="list_item_info_house"><?php if($row['publishing_house'] == ""){ echo ""; } else {echo "<b>Publishing house</b> ".$row['publishing_house'];}?></div>
+                    <div class="list_item_info_original"><?php if ($row['original_title'] == ""){ echo "";} else { echo "<b>Original title</b> <i>".$row['original_title'] . "</i>";}?></div>
+                </div>
+                <!-- <div id="column-3">
+                    <img class="list_item_info_cover" src="assets/covers/placeholder.png"/>
+                </div> -->
             </div>
         </div>
         <?php
